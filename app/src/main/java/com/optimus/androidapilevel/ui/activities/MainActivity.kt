f234a5ad1c69418aeed945f78590a9551fdbb85a
Dmitriy Chebotar <optimussflash@ukr.net>
2020-02-03 00:21:54 +0200

    refactoring options menu

--- app/src/main/java/com/optimus/androidapilevel/ui/activities/MainActivity.kt
+++ app/src/main/java/com/optimus/androidapilevel/ui/activities/MainActivity.kt
@@ -2,7 +2,11 @@ package com.optimus.androidapilevel.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.optimus.androidapilevel.R
+
 import com.optimus.androidapilevel.ui.adapters.ApiLevelPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,5 +20,21 @@ class MainActivity : AppCompatActivity() {
         val apiLevelPagerAdapter = ApiLevelPagerAdapter(supportFragmentManager)
         view_pager.adapter = apiLevelPagerAdapter
         tab_layout.setupWithViewPager(view_pager)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.settings -> {
+                Toast.makeText(this, "Some settings...", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 }
